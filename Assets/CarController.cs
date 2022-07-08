b456449b81ea762fe59032d99d34a83745bd4272
Jaroslaw Gawel <audikrk@interia.pl>
2022-07-08 21:10:06 +0200

    CarSystem Implemented but on AWSD

---
+++ Assets/CarController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float MaxMotorTorque;
+    public float MaxSteeringAngle;
+
+    public void FixedUpdate()
+    {
+        float motor = MaxMotorTorque * Input.GetAxis("Vertical");
+        float steering = MaxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach(AxleInfo axleInfo in axleInfos)
+        {
+            if(axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if(axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
